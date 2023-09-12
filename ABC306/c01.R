7ff8c85e4aa28b2800fa7159ca3e4c3b772f53cb
Kenta_Nakajima <knknkn1162@gmail.com>
2023-09-12 19:44:50 +0900

    solve C in ABC306

---
+++ ABC306/c01.R
@@ -0,0 +1,13 @@
+options(scipen=100)
+conn <- file("stdin", open="r")
+#conn <- stdin()
+N <- readLines(conn, n=1) |> parse_number()
+A <- readLines(conn, n=1) |> str_split_1(" ") |> parse_number()
+
+A |>
+  as_tibble() |>
+  mutate(idx = row_number()) |>
+  summarize(mid = nth(idx, 2), .by = value) |>
+  arrange(mid) |>
+  pull(value) |>
+  cat()
