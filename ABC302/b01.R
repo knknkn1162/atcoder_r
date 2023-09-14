33a50235d6d4dcbc08a222ca46a932469b1e5b66
Kenta_Nakajima <knknkn1162@gmail.com>
2023-09-14 16:08:31 +0900

    solve B in ABC302
    but it's too slow

---
+++ ABC302/b01.R
@@ -0,0 +1,42 @@
+# tidyverse, bigint
+options(scipen=100)
+#conn <- file("stdin", open="r")
+conn <- stdin()
+inputs <- readLines(conn, n=1) |> str_split_1(" ") |> parse_number()
+
+H <- inputs[1]; W <- inputs[2]
+inputs <- readLines(conn, n=H)
+dist = list(
+  tibble(dx = 0, dy = 0:4),
+  tibble(dx = 0, dy = 0:-4),
+  tibble(dx = 0:4, dy = 0),
+  tibble(dx = 0:-4, dy = 0),
+  tibble(dx = 0:4, dy = 0:4),
+  tibble(dx = 0:4, dy = 0:-4),
+  tibble(dx = 0:-4, dy = 0:4),
+  tibble(dx = 0:-4, dy = 0:-4)
+)
+tb <- inputs |>
+  str_split_fixed("",n = W) |>
+  as_tibble() |>
+  mutate(row = row_number()) |>
+  pivot_longer(-row, names_to="col") |>
+  mutate(col = parse_number(col))
+res <- list()
+for(h in 1:H) {
+  for(w in 1:W) {
+    res <- append(res,
+      dist |>
+        map(\(dtb) 
+          dtb |>
+            mutate(row = h + dy, col = w + dx) |>
+            left_join(tb) |>
+            drop_na()
+        )
+    )
+  }
+}
+res |>
+  detect(\(dtb) dtb |> summarize(str = str_flatten(value)) |> pull() |> str_detect("snuke")) |>
+  select(row, col) |>
+  write_delim(stdout(), col_names=FALSE)
