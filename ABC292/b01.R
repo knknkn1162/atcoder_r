992821e4c61e03b7c3b594172d1c22cd45ffc288
Kenta_Nakajima <knknkn1162@gmail.com>
2023-09-18 07:44:46 +0900

    fix

--- ABC292/b01.R
+++ ABC292/b01.R
@@ -1,7 +1,7 @@
 # tidyverse, sets
 options(scipen=100)
-#conn <- file("stdin", open="r")
-conn <- stdin()
+conn <- file("stdin", open="r")
+#conn <- stdin()
 
 inputs <- readLines(conn, n=1) |> str_split_1(" ") |> parse_number()
 N <- inputs[1]; Q <- inputs[2]
