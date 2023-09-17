326ae97de34d013c3766f103d4bd0a8c27755f46
Kenta_Nakajima <knknkn1162@gmail.com>
2023-09-17 15:28:50 +0900

    solve A in ABC294

---
+++ ABC294/a01.R
@@ -0,0 +1,14 @@
+# slider
+options(scipen=100)
+conn <- file("stdin", open="r")
+#conn <- stdin()
+
+N <- readLines(conn, n=1) |> parse_number()
+A <- readLines(conn, n=1) |> str_split_1(" ") |> parse_number()
+
+
+A |>
+  as_tibble() |>
+  filter(value %% 2 == 0) |>
+  pull(value) |>
+  cat()
