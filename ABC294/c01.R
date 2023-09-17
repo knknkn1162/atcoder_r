a468e1af4390b5168e002b8c196973d9b5c161e3
Kenta_Nakajima <knknkn1162@gmail.com>
2023-09-17 15:29:06 +0900

    solve C in ABC294

---
+++ ABC294/c01.R
@@ -0,0 +1,26 @@
+# tidyverse, utils
+options(scipen=100)
+conn <- file("stdin", open="r")
+#conn <- stdin()
+
+inputs <- readLines(conn, n=1) |> str_split_1(" ") |> parse_number()
+N <- inputs[1]; M <- inputs[2]
+
+A <- readLines(conn, n=1) |> str_split_1(" ") |> parse_number()
+B <- readLines(conn, n=1) |> str_split_1(" ") |> parse_number()
+
+dummy <- A |>
+  as_tibble() |>
+  mutate(label = "A") |>
+  bind_rows(
+    B |> as_tibble() |> mutate(label = "B") 
+  ) |>
+  arrange(value) |>
+  mutate(idx = row_number()) |>
+  group_by(label) |>
+  group_map(\(stb,key) stb |>
+    pull(idx) |>
+    str_flatten(" ")
+  ) |>
+  unlist() |>
+  cat(sep ="\n")
