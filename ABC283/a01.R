628d07b864dfafbf4ae50871d5d4fa767f74a798
Kenta_Nakajima <knknkn1162@gmail.com>
2023-09-24 12:56:50 +0900

    solve A in ABC283

---
+++ ABC283/a01.R
@@ -0,0 +1,9 @@
+options(scipen=100)
+conn <- file("stdin", open="r")
+#conn <- stdin()
+
+inputs <- readLines(conn, n=1) |> str_split_1(" ") |> parse_number()
+A <- inputs[1]; B <- inputs[2]
+
+A^B |>
+  cat()
