768d752c19c876e388ed5a7668b01e8ba012827d
Kenta_Nakajima <knknkn1162@gmail.com>
2023-10-07 14:08:48 +0900

    solve C in ABC277

---
+++ ABC277/c01.R
@@ -0,0 +1,54 @@
+{
+# tidyverse, utils
+options(scipen=100)
+conn <- file("stdin", open="r")
+#conn <- stdin()
+
+N <- readLines(conn, n=1) |> parse_number()
+
+tb <- read.table(conn, nrows=N) |>
+  as_tibble()
+
+gtb <- tb |>
+  (\(x) add_row(x, tibble(V1=x$V2, V2=x$V1)))() |>
+  nest(.by=V1) |>
+  (\(x) hash::hash(x$V1, x$data))()
+
+if(!hash::has.key("1", gtb)) {
+  cat(1)
+  zzzzzz()
+}
+
+htable <- tb |>
+  unlist() |>
+  sort() |>
+  unique() |>
+  (\(x) hash::hash(x, FALSE))()
+# initialize
+lst <- c(1)
+while(1) {
+  # modify
+  for(val in lst) {
+    htable[[as.character(val)]] = TRUE
+  }
+  # update
+  lst <- lst |>
+    as.character() |>
+    map(\(val) gtb[[val]]) |>
+    list_rbind() |>
+    distinct() |>
+    rowwise() |>
+    filter(!htable[[as.character(V2)]]) |>
+    pull()
+  if(length(lst) == 0) {
+    break
+  }
+}
+ans <- 1
+for(key in hash::keys(htable)) {
+  if(htable[[key]]) {
+    ans <- max(ans, as.numeric(key))
+  }
+}
+cat(ans)
+}
